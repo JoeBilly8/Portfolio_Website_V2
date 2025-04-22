d65c6476f9c9461de387ac868afd7625bdc214eb
JoeBilly8 <joebilly8@hotmail.co.uk>
2025-04-22 16:09:50 +1000

    updated SVGs and added buttons

--- dist/js/add-button.js
+++ dist/js/add-button.js
@@ -1,5 +1,7 @@
 const button = document.getElementById("add-button");
+const displayButtons = document.getElementsByClassName("select-buttons")[0];
 
 button.addEventListener("click", () => {
     button.classList.toggle("active");
+    displayButtons.classList.toggle("active");
 });
