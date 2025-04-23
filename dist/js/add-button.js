d345cbf13dce72dcf7d910f54603f5d6abb8abc0
JoeBilly8 <joebilly8@hotmail.co.uk>
2025-04-23 18:29:13 +1000

    changes to add the option buttons with smooth sexy animation

--- dist/js/add-button.js
+++ dist/js/add-button.js
@@ -1,7 +1,27 @@
 const button = document.getElementById("add-button");
 const displayButtons = document.getElementsByClassName("select-buttons")[0];
 
+let open = false;
 button.addEventListener("click", () => {
-    button.classList.toggle("active");
-    displayButtons.classList.toggle("active");
+
+    console.log("button clicked and open was " + open);
+
+    if (!open) {
+        button.classList.add("active");
+        displayButtons.classList.add("active");
+        displayButtons.classList.remove("closing");
+    } else {
+        displayButtons.classList.add("closing");
+        button.classList.remove("active");
+
+        setTimeout(() => {
+            displayButtons.classList.remove("closing");
+            displayButtons.classList.remove("active");
+        }, 400);
+    }
+
+    open = !open;
+
+    console.log("open is now " + open);
+
 });
